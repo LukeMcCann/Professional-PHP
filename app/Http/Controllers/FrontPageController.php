8f06531449f2a562220813df5cc9366b45a7c962
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-06 22:26:28 +0000

    Add dependency injection interface example

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Core\Rendering\TemplateRenderer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class FrontPageController 
 {
+    private $templateRenderer; 
+
+    public function __construct(TemplateRenderer $templateRenderer)
+    {
+        $this->templateRenderer = $templateRenderer;
+    }
+
     public function show(Request $request) : Response
     {
         $content = 'Hello, ' . $request->get('name', 'visitor');
